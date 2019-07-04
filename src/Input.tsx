d696ae9760971c887013f07a6325c4cf52363cab
zynkn <ywnwalone@naver.com>
2019-07-05 00:31:52 +0900

    UI 완료!

--- src/Input.tsx
+++ src/Input.tsx
@@ -4,18 +4,29 @@ import { MdClear } from 'react-icons/md';
 import { FaEyeSlash, FaEye } from 'react-icons/fa';
 
 const Input: React.FC<any> = (props) => {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const [inputType, setInputType] = React.useState("password");
 
+  const handleVisible = () => {
+    setIsVisible(!isVisible);
+    setInputType(inputType === "text" ? "password" : "text");
+  }
   return (
-    <label className="Input">
-      <input type="text" />
-      <span className="label">Label</span>
-      {/* <button className="delete-btn"><MdClear /></button> */}
-      <button className="delete-btn"><FaEyeSlash /></button>
-      <div className="validator">
-        <span></span>
-        <span></span>
-        <span></span>
+    <label className="Input" htmlFor="password">
+      <span className="label-text">ENTER PASSWORD</span>
+      <div className="input-wrap">
+        <input type={inputType} id="password" />
+        <button className="toggle-btn" onClick={handleVisible}>
+          {isVisible ? <FaEye /> : <FaEyeSlash />}
+        </button>
       </div>
+      <div className="validator">
+        <span className="bar active" />
+        <span className="bar active" />
+        <span className="bar" />
+        <span className="validator-text">Could be stronger</span>
+      </div>
+
     </label>
   )
 }
